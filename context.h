d60f58d1d5eb9005b15e17b86cbbab77aebfcfe8
GengZhengLin <gzl8042@163.com>
2015-01-20 09:37:08 +0800

    test git

--- context.h
+++ context.h
@@ -4,6 +4,6 @@ struct Context
 	int width;
 	int height;
 };
-
+//context 函数接口
 void init_context(struct Context* context_ptr, int width, int height);
 void free_context(struct Context* context_ptr);
