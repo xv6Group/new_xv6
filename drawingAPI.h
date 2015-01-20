5fb00f7b14baf4ecee3e59e58bdeacc8db1316c4
liutongtong <jshaltt7@163.com>
2015-01-20 15:25:02 +0800

    drawingAPI modify

--- drawingAPI.h
+++ drawingAPI.h
@@ -17,4 +17,6 @@ struct Context;
 void draw_point(struct Context c, unsigned int x, unsigned int y, unsigned short color);
 void fill_rect(struct Context c, unsigned int bx, unsigned int by, unsigned int width, unsigned int height, unsigned short color);
 void puts_str(struct Context c, char *str, unsigned short colorNum, int x, int y);
+void initializeHankaku();
+
 #endif
