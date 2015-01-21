4568c4f01475d37f4c3819359898105d281dbc8e
GengZhengLin <gzl8042@163.com>
2015-01-21 16:30:44 +0800

    local changes

--- finder.c
+++ finder.c
@@ -1,6 +1,7 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "fs.h"
 #include "context.h"
 #include "drawingAPI.h"
 #include "message.h"
@@ -9,40 +10,86 @@
 
 struct Context context;
 
+char*
+fmtname(char *path)
+{
+  static char buf[DIRSIZ+1];
+  char *p;
+
+  // Find first character after last slash.
+  for(p=path+strlen(path); p >= path && *p != '/'; p--)
+    ;
+  p++;
+
+  // Return blank-padded name.
+  if(strlen(p) >= DIRSIZ)
+    return p;
+  memmove(buf, p, strlen(p));
+  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+  return buf;
+}
+
+void
+ls(char *path)
+{
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+
+  if((fd = open(path, 0)) < 0){
+    printf(2, "ls: cannot open %s\n", path);
+    return;
+  }
+
+  if(fstat(fd, &st) < 0){
+    printf(2, "ls: cannot stat %s\n", path);
+    close(fd);
+    return;
+  }
+
+  switch(st.type){
+  case T_FILE:
+    printf(1, "%s %d %d %d\n", fmtname(path), st.type, st.ino, st.size);
+    break;
+
+  case T_DIR:
+    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+      printf(1, "ls: path too long\n");
+      break;
+    }
+    strcpy(buf, path);
+    p = buf+strlen(buf);
+    *p++ = '/';
+    while(read(fd, &de, sizeof(de)) == sizeof(de)){
+      if(de.inum == 0)
+        continue;
+      memmove(p, de.name, DIRSIZ);
+      p[DIRSIZ] = 0;
+      if(stat(buf, &st) < 0){
+        printf(1, "ls: cannot stat %s\n", buf);
+        continue;
+      }
+      printf(1, "%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
+    }
+    break;
+  }
+  close(fd);
+}
+
 int main(int argc, char *argv[]) {
 	int winid;
 	struct Msg msg;
 	int isRun = 1;
-	int pid;
-	int isInit = 1;
 
 	winid = init_context(&context, 400, 300);
-
 	fill_rect(context, 0, 0, context.width, context.height, 0xf800);
-
 	draw_window(context, "finder");
 
 	while (isRun) {
 		getMsg(&msg);
 		switch (msg.msg_type) {
 		case MSG_UPDATE:
-
-			if (isInit) {
-				printf(1, "init ls: starting ls\n");
-				pid = fork();
-				if (pid < 0) {
-					printf(1, "init ls: ls failed\n");
-					exit();
-				}
-				if (pid == 0) {
-					exec("ls", argv);
-					printf(1, "init ls: exec ls failed\n");
-					exit();
-				}
-				isInit = 0;
-			}
-
-
 			updateWindow(winid, context.addr);
 			break;
 		default:
